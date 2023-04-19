ebc0d3b79e55945f99bbafc3548f3fde50a5d4a1
Alessio Greggi <ale_grey_91@hotmail.it>
2023-04-19 17:10:32 +0200

    fix: move test to deeper function
    Signed-off-by: Alessio Greggi <ale_grey_91@hotmail.it>

--- sensor/controlplane_test.go
+++ sensor/controlplane_test.go
@@ -1,13 +1,9 @@
 package sensor
 
 import (
-	"context"
 	"encoding/json"
-	"strings"
 	"testing"
 
-	"github.com/kubescape/host-scanner/sensor/internal/utils"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -85,36 +81,6 @@ func Test_removeEncryptionProviderConfigSecrets(t *testing.T) {
 			removeEncryptionProviderConfigSecrets(data)
 			v, _ := json.Marshal(data)
 			require.JSONEq(t, tt.output, string(v))
-		})
-	}
-}
-
-func Test_makeAPIserverAuditPolicyFile(t *testing.T) {
-	uid_tests := []struct {
-		name     string
-		cmdLine  string
-		expected int
-	}{
-		{
-			name:    "audit_policy_file_exist",
-			cmdLine: "kube-apiserver --audit-policy-file=./testdata/audit-policy.yaml",
-		},
-		{
-			name:    "audit_policy_file_exist",
-			cmdLine: "kube-apiserver",
-		},
-	}
-
-	for _, tt := range uid_tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ctx := context.Background()
-			process := &utils.ProcessDetails{
-				CmdLine: strings.Split(tt.cmdLine, " "),
-			}
-			fileInfo := makeAPIserverAuditPolicyFile(ctx, process)
-			if assert.NotEqual(t, fileInfo, nil) {
-				t.Log("err")
-			}
 		})
 	}
 }
